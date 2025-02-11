fd187b2f338588232b1836b3d33d25de2ce89d6f
signesunekaer <signesunekaer@outlook.dk>
2025-02-11 11:09:58 +0100

    rykkede js

--- projekt4.js
+++ projekt4.js
@@ -162,6 +162,12 @@ let shirt = [];
 
 let theShirt = { name: "LANGÆRMET TRØJE", price: 599.95 };
 
+shirt.push(theShirt);
+
+console.log(shirt);
+
+// FØJ TIL KURV SLUT
+
 
 
 // Funktion til at tilføje til favoritter //
@@ -219,8 +225,3 @@ document.addEventListener("DOMContentLoaded", function () {
 
     updateFavorites(); // Indlæs favoritter fra localStorage ved load
 });
-shirt.push(theShirt);
-
-console.log(shirt);
-
-// FØJ TIL KURV SLUT
